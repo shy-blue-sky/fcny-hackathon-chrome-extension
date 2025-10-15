31c7aab5fcdde6abc044c1a8450992cf74f53b9c
shy-blue-sky <hyeyoonsung@gmail.com>
2025-10-15 17:37:32 -0400

    First draft

---
+++ main.js
@@ -0,0 +1,42 @@
+const wordsOfAffirmation = []; // Mood check: Positive mental health quotes
+// const timedTaskReminder = []; // Maybe should be an object, so that it can store the time as well?
+let timedTaskReminder = {};
+
+let character = [];
+
+// Loop through words randomly
+for (quotes in wordsOfAffirmation) {
+    const index = Math.floor(Math.random() * coverUp.length);
+    return wordsOfAffirmation[index]; // Need to display words
+} 
+
+//"Animate" character by looping through random poses/images of character?  ---> Could be a stretch goal
+
+//For letting user manually input task links & timer they want to be reminded of later
+let userTaskLink;
+let userTaskTime;
+
+// timedTaskReminder.push(userTaskLink)
+timedTaskReminder[userTaskTime] = userTaskLink; // As a key-pair value?
+
+function taskInputPopup() { // Maybe could be done via DOM manipulation
+    if (!userTaskTime) {
+     console.log("Hi! Please add a link that you'd like to be reminded to revisit later :)");
+    
+    }
+
+    else {
+    console.log("Hi! Please add the time you'd like to be reminded by! The format should be HH:MM.");
+    if (userTaskTime ) // if input time has already passed that day {
+    console.log("Sorry, seems like it's already past that time today!")
+
+} else if ()
+    }
+}
+
+
+function taskRemindPopup() { // Should do via DOM
+    return "Hey, seems like it's time to check this link out:" + userTaskLink;
+}
+
+
