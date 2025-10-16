54c9a918d91e2d35ff762ae466b8b7110f625a34
shy-blue-sky <hyeyoonsung@gmail.com>
2025-10-16 11:40:40 -0400

    Backup of our work (pre-Bulma)

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-const wordsOfAffirmation = []; // Mood check: Positive mental health quotes
+// Mood check: Positive mental health quotes
+const wordsOfAffirmation = [
+    "Hi there! Don't forget to dehydrate :)", 
+    "Take a little walk sometimes!"]; 
+
+// For reminding user of tasks at hand; saves both the 'timer' and 'task (link to read)'
 // const timedTaskReminder = []; // Maybe should be an object, so that it can store the time as well?
 let timedTaskReminder = {};
 
-let character = [];
+// For character image
+let character = ["https://cdn.wikirby.com/thumb/0/0d/KARs_Kirby.png/1293px-KARs_Kirby.png?20250819145246"];
+
+// For character image changing/update
+//"Animate" character by looping through a number of poses/images of character?  ---> Could be a stretch goal
+const imgs = document.getElementsByClassName("characs")
+
+for (image of imgs) {
+    const index = Math.floor(Math.random() * character.length);
+    image.src = character[index];
+}
+
+
 
 // Loop through words randomly
-for (quotes in wordsOfAffirmation) {
-    const index = Math.floor(Math.random() * coverUp.length);
-    return wordsOfAffirmation[index]; // Need to display words
-} 
 
-//"Animate" character by looping through random poses/images of character?  ---> Could be a stretch goal
+// for (quotes in wordsOfAffirmation) {
+//     const index = Math.floor(Math.random() * wordsOfAffirmation.length);
+//     return wordsOfAffirmation[index]; // Need to display words
+// } 
+
 
 //For letting user manually input task links & timer they want to be reminded of later
 let userTaskLink;
@@ -19,20 +36,48 @@ let userTaskTime;
 // timedTaskReminder.push(userTaskLink)
 timedTaskReminder[userTaskTime] = userTaskLink; // As a key-pair value?
 
-function taskInputPopup() { // Maybe could be done via DOM manipulation
-    if (!userTaskTime) {
-     console.log("Hi! Please add a link that you'd like to be reminded to revisit later :)");
+// function taskInputPopup() { // Maybe could be done via DOM manipulation
+//     if (!userTaskTime) {
+//      console.log("Hi! Please add a link that you'd like to be reminded to revisit later :)");
     
-    }
+//     }
 
-    else {
-    console.log("Hi! Please let us know when you'd like to be reminded by! The format should be HH:MM."); // Set timer
-    if (userTaskTime ) // if input time has already passed that day {
-    console.log("Sorry, seems like it's already past that time today!")
+//     else {
+//     console.log("Hi! Please let us know when you'd like to be reminded by! The format should be HH:MM."); // Set timer
+//     if (userTaskTime ) // if input time has already passed that day {
+//     console.log("Sorry, seems like it's already past that time today!")
 
-} else if ()
-    }
-}
+// } else if ()
+//     }
+// }
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const focusInput = document.getElementById("focusInput");
+    const saveFocusButton = document.getElementById("saveFocus");
+    const promptUser = document.getElementById("prompt");
+
+    saveFocusButton.addEventListener('click', function() {
+        const focusLink = focusInput.value;
+        if(focusLink) {
+            chrome.storage.sync.set({"savedLink": focusLink}, function() {
+            promptUser.textContent = "Link to remind you later, saved!";
+            setTimeout(() => promptUser.textContent = '', 850);
+          });
+        } else {
+            promptUser.textContent = "Please save a link you'd like to revisit later!"
+        }
+    });
+
+    //   // Optionally, load a previously saved value
+    //   chrome.storage.sync.get('savedLink', function(data) {
+    //     if (data.savedValue) {
+    //       focusInput.value = data.savedLink;
+    //     }
+    //   });
+
+})
+
 
 
 function taskRemindPopup() { // Should do via DOM
